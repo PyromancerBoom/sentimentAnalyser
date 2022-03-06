2d300c3c6b52edcfb369bc85f06705679a6c6a73
PyromancerBoom <priyanshmagic@gmail.com>
2022-03-06 10:00:13 +0530

    twitter analysis

--- twitterAnalysis.py
+++ twitterAnalysis.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 
 def get_tweets():
     import GetOldTweets3 as got
-    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('CoronaOutbreak') \
-        .setSince("2020-01-01") \
-        .setUntil("2020-04-01") \
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('LGBTQ+') \
+        .setSince("2020-03-01") \
+        .setUntil("2022-03-03") \
         .setMaxTweets(1000)
     # Creation of list that contains all tweets
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
